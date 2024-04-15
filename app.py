2e75a0237f57db9619726a3ac92883ca46d51d0b
panka <kacper.wojcik@edu.uekat.pl>
2024-04-15 16:15:21 +0200

    połączenia miedzu stronami

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
-from flask import Flask, redirect, render_template, url_for, request, session
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import login_required, error
-from sqlalchemy.sql.expression import func
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///glodny.db'
-db=SQLAlchemy(app)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/posty")
-def end():
-    return render_template("posty.html")
+@app.route('/komentarze')
+def page1():
+    return render_template('komentarze.html')
 
-@app.route("/komentarze")
-def end():
-    return render_template("komentarze.html")
+@app.route('/albumy')
+def page2():
+    return render_template('albumy.html')
 
-@app.route("/albumy")
-def end():
-    return render_template("albumy.html")
+@app.route('/zdjecia')
+def page2():
+    return render_template('zdjecia.html')
 
-@app.route("/zdjecia")
-def end():
-    return render_template("zdjecia.html")
+@app.route('/posty')
+def page2():
+    return render_template('posty.html')
